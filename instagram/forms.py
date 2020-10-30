c453506216362615c906f73e9ce4c3c9da53827d
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-10-30 11:15:43 +0300

    deployment

--- instagram/forms.py
+++ instagram/forms.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ['user']
+        exclude = ['user','following']
 
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['likes', 'posted', 'profile', 'profile_det', 'comments']
+        exclude = ['likes', 'name', 'profile', 'comments']
 
 class CommentForm(forms.ModelForm):
     class Meta:
